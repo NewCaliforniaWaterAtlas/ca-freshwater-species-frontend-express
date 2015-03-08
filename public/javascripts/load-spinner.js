3dcc7b6ff3bc457a32931de2a0d423e82c11d858
Eric Theise <erictheise@gmail.com>
2015-03-08 16:51:13 -0700

    larger, better

--- public/javascripts/load-spinner.js
+++ public/javascripts/load-spinner.js
@@ -1,10 +1,17 @@
 (function (global, $) {
+  var spin_opts = {
+    radius: 48,   // inner radius
+    length: 32,   // extension beyond radius
+    width: 16,
+    shadow: true
+  };
+
   $('#map').append('\
     <div id="ajax-spinner"></div> \
   ');
 
   $(global).ajaxStart(function() {
-    $('#ajax-spinner').spin();
+    $('#ajax-spinner').spin(spin_opts);
   });
 
   $(global).ajaxComplete(function() {
